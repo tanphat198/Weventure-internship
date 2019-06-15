39a76a96c6721cb61327c0086727708dc4553971
tanphat198 <44112014+tanphat198@users.noreply.github.com>
2019-06-15 08:32:13 +0700

    Update ExchangeRatesUnitTest.py
    update body test

--- ExchangeRates/ExchangeRatesUnitTest.py
+++ ExchangeRates/ExchangeRatesUnitTest.py
@@ -15,7 +15,8 @@ class test_function(unittest.TestCase):
     
     def test_request_latest_by_using_httprety(self):
         httpretty.activate()
-        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/latest", body = "ahaha this is new body")
+        body_from_latest = '{"base":"EUR","rates":{"BGN":1.9558},"date":"2019-06-14"}'
+        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/latest", body = body_from_latest)
         Response_from_main = exchangeService("https://api.exchangeratesapi.io").get_latest()
         fake_response = requests.request("GET" , url = "https://api.exchangeratesapi.io/latest" )
         self.assertEqual(Response_from_main,fake_response.text)
@@ -28,7 +29,8 @@ class test_function(unittest.TestCase):
 
     def test_request_time_by_using_httprety(self):
         httpretty.activate()
-        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/2007-12-20", body = "ahaha this is new body")
+        body_from_time = '{"base":"EUR","rates":{"BGN":1.9558},"date":"2007-12-20"}'
+        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/2007-12-20", body = body_from_time)
         Response_from_main = exchangeService("https://api.exchangeratesapi.io").get_by_time('20' , '12' , '2007')
         fake_response = requests.request("GET" , url = "https://api.exchangeratesapi.io/2007-12-20" )
         self.assertEqual(Response_from_main,fake_response.text)
@@ -42,7 +44,8 @@ class test_function(unittest.TestCase):
    
     def test_reqest_base_by_using_httprety(self):
         httpretty.activate()
-        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/latest?base=USD", body = "ohoho this is new body")
+        body_from_base = '{"base":"EUR", "rates":{"BGN":1.6989}}'
+        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/latest?base=USD", body = body_from_base)
         Response_from_main = exchangeService("https://api.exchangeratesapi.io").get_by_base('USD')
         fake_response = requests.request("GET" , url = "https://api.exchangeratesapi.io/latest?base=USD" )
         self.assertEqual(Response_from_main,fake_response.text)
@@ -55,7 +58,8 @@ class test_function(unittest.TestCase):
 
     def test_reqest_symbols_by_using_httprety(self):
         httpretty.activate()
-        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/latest?symbols=USD,GBP", body = "ohoho this is new body")
+        body_from_symbols = '{"base":"EUR", "rates":{"BGN":1.6989}, "symbols" : {"ILS","JPY"}}'
+        httpretty.register_uri(httpretty.GET, "https://api.exchangeratesapi.io/latest?symbols=USD,GBP", body = body_from_symbols)
         Response_from_main = exchangeService("https://api.exchangeratesapi.io").get_by_symbols("USD","GBP")
         fake_response = requests.request("GET" , url = "https://api.exchangeratesapi.io/latest?symbols=USD,GBP" )
         self.assertEqual(Response_from_main,fake_response.text)
