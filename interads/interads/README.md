7bc258bd09f55ae5e2b2a634a0484bf0bfd75348
tanphat198 <44112014+tanphat198@users.noreply.github.com>
2019-06-19 15:06:30 +0700

    update

--- interads/interads/README.md
+++
@@ -1,5 +0,0 @@
-  Install offline:
-pip intall interads-0.0.1.tar.gz
-    Install qua pypi:
-pip install interads
-
--- interads/interads/cores/delegation/responsehandler.py
+++
@@ -1,7 +0,0 @@
-class ResponseHandler(object):
-    def handle(self, response):
-        pass
-
-    def on_error(self, error, request):
-        pass
-
