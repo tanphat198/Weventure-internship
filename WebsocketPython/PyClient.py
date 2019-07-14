713df95b01e18dcf11a3a756386755380013af20
Phat Nguyen Tan <44112014+tanphat198@users.noreply.github.com>
2019-07-14 13:34:06 +0700

    Demo Client-Server Websocket python

---
+++ WebsocketPython/PyClient.py
@@ -0,0 +1,17 @@
+import asyncio
+import websockets
+
+import json
+
+
+listfruits = '{"orange":"Qua cam", "strawberry":"Day tay", '\
+             '"grape":"Nho", "durian":"Sau rieng"}'
+
+async def message():
+    async with websockets.connect("ws://localhost:8000") as socket:
+        msg = listfruits
+        await socket.send(msg)
+        print(await socket.recv())
+
+
+asyncio.get_event_loop().run_until_complete(message())
---
+++ WebsocketPython/PyServer.py
@@ -0,0 +1,12 @@
+import asyncio
+import websockets
+
+
+async def response(websocket, path):
+    message = await websocket.recv()
+    print("We got the message from the client: ",message)
+    await websocket.send("server got your message!")
+
+start_server = websockets.serve(response, 'localhost', 8000)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
