fd91e2dfb7bfb34d979c4e67fa2e83f5add90070
Phat Nguyen Tan <44112014+tanphat198@users.noreply.github.com>
2019-07-14 13:35:08 +0700

    Update PyServer.py

--- WebsocketPython/PyServer.py
+++ WebsocketPython/PyServer.py
@@ -10,3 +10,4 @@ async def response(websocket, path):
 start_server = websockets.serve(response, 'localhost', 8000)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
